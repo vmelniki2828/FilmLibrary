04e334899b233e9653dda7f669e9ad363be20c7e
Vlad Melnik <vmelniki2828@gmail.com>
2023-04-24 15:28:45 +0300

    footer adaptiv

--- src/js/to_top.js
+++ src/js/to_top.js
@@ -1,21 +1,21 @@
-const goTopBtn = document.querySelector('.to_top')
+const goTopBtn = document.querySelector('.to_top');
 
-goTopBtn.addEventListener('click', goTop)
-window.addEventListener("scroll", trackScroll)
+goTopBtn.addEventListener('click', goTop);
+window.addEventListener('scroll', trackScroll);
 
-function trackScroll(){
-    const offset = window.pageYOffset
-    const coords = document.documentElement.clientHeight;
-    if(offset < coords){
-        goTopBtn.classList.remove("go_top_show")
-    }else{
-        goTopBtn.classList.add("go_top_show")
-    }
+function trackScroll() {
+  const offset = window.pageYOffset;
+  const coords = document.documentElement.clientHeight;
+  if (offset < coords) {
+    goTopBtn.classList.remove('go_top_show');
+  } else {
+    goTopBtn.classList.add('go_top_show');
+  }
 }
 
-function goTop(){
-    if(window.pageYOffset>0){
-        window.scrollBy(0, -75)
-        setTimeout(goTop, 0)
-    }
+function goTop() {
+  if (window.pageYOffset > 0) {
+    window.scrollBy(0, -75);
+    setTimeout(goTop, 0);
+  }
 }
