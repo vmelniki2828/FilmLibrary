e5334afbb012cefb1e00216bc5008b3a05cdd975
DanMonday <124054149+DanMonday@users.noreply.github.com>
2023-04-29 01:07:57 +0300

    local storag, modal

--- src/js/active_btn.js
+++ src/js/active_btn.js
@@ -1,15 +1,52 @@
+const IMG_URL = 'https://image.tmdb.org/t/p/original/';
+import idToGenre from './film_list';
+
 const refs = {
   btnWatched: document.querySelector('.btn_watched'),
   btnQueue: document.querySelector('.btn_queue'),
+  mainFilm: document.querySelector('.movies_librery')
 };
+
 
 refs.btnQueue.addEventListener('click', () => {
     refs.btnWatched.classList.remove('is_active_btn');
     refs.btnQueue.classList.add('is_active_btn');
+
 })
 
 refs.btnWatched.addEventListener('click', () => {
   refs.btnQueue.classList.remove('is_active_btn');
   refs.btnWatched.classList.add('is_active_btn');
+
+  const movieList = localStorage.getItem('itemWatched');
+  const parseMovieList = JSON.parse(movieList);
+  parseMovieList.map(movie => {
+    const {
+      title,
+      poster_path,
+      vote_average,
+      genre_ids,
+      release_date = '',
+    } = movie;
+    const movieEl = document.createElement('li');
+    movieEl.classList.add('movie_card');
+    movieEl.innerHTML = `
+    <div class="image-container">
+        <img class="movie__img" src="${
+          poster_path === null ? noImg : IMG_URL + poster_path
+        }" alt="${title}">
+        </div>
+        <h3 class="movie_title">${title}</h3>
+        <div class="movie_info">
+            <span class="movie_genre">${
+              idToGenre(genre_ids) + ' | ' + release_date.slice(0, 4)
+            }</span>
+
+            <span class="movie_vote">${vote_average}</span>
+
+        </div>
+        `;
+    refs.mainFilm.appendChild(movieEl);
+  })
 });
 
--- src/js/switcher_btn.js
+++ src/js/switcher_btn.js
@@ -5,6 +5,7 @@ const refs = {
   slider: document.querySelector('.glide'),
   form: document.querySelector('.form'),
   mainMovie: document.querySelector('.main_movies'),
+  mainMovieLibrary: document.querySelector('.main_movies_library'),
   btn: document.querySelector('.head_librery'),
   headSection: document.querySelector('.header__section'),
 }; 
@@ -19,6 +20,7 @@ home_library[0].addEventListener('click', ()=>{
     refs.slider.classList.remove('is_hidden');
     refs.form.classList.remove('is_hidden')
     refs.mainMovie.classList.remove('is_hidden')
+    refs.mainMovieLibrary.classList.add('is_hidden');
     refs.btn.classList.add('is_hidden');
     refs.headSection.classList.add('header__section');
     refs.headSection.classList.remove('header_bg-section');
@@ -31,6 +33,7 @@ home_library[1].addEventListener('click', ()=>{
     refs.slider.classList.add('is_hidden');
     refs.form.classList.add('is_hidden')
     refs.mainMovie.classList.add('is_hidden');
+    refs.mainMovieLibrary.classList.remove('is_hidden');
     refs.btn.classList.remove('is_hidden');
     refs.headSection.classList.remove('header__section');
     refs.headSection.classList.add('header_bg-section');
